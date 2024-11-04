324ff0a9d656cb420fc014130b2d086ebde819ff
Vanish0314 <2013107081@qq.com>
2024-11-04 15:47:16 +0800

    fbo

--- Source/Src/main.cpp
+++ Source/Src/main.cpp
@@ -1,7 +1,7 @@
 /*
  * @Author: Vanish
  * @Date: 2024-09-09 21:35:01
- * @LastEditTime: 2024-10-21 16:09:51
+ * @LastEditTime: 2024-11-04 15:47:12
  * Also View: http://vanishing.cc
  * Copyright@ https://creativecommons.org/licenses/by/4.0/deed.zh-hans
  */
@@ -35,7 +35,7 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 //     return window;
 // }
 
-int main()
+GLFWwindow* CreateWindow(int width, int height, const char* title,GLFWframebuffersizefun callback)
 {
     //初始化glfw
     glfwInit(); 
@@ -66,6 +66,10 @@ int main()
     glViewport(0, 0, 800, 800);//左下角坐标为(0,0),右上角坐标为(800,600)
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);//设置窗口大小变化回调函数
 
+    return window;
+}
+void HellowWorld()
+{
     std::string startTitle = 
         "|********************************************|\n"
         "|************BickRenderer start**************|\n"
@@ -73,6 +77,19 @@ int main()
         "|********************************************|";
 
     std::cout <<"\n\n\n"<< startTitle << std::endl<<std::endl;
+}
+void SetOpenGL()
+{
+    glEnable(GL_DEPTH_TEST); // 启用深度测试
+    glDepthFunc(GL_LESS);    // 设置深度测试函数为 LESS
+    glDepthMask(GL_TRUE);    // 启用深度写入
+    glDisable(GL_CULL_FACE); // 禁用背面剔除
+    glEnable(GL_BLEND); // 启用混合
+}
+int main()
+{
+    GLFWwindow *window = CreateWindow(800, 600, "BickRenderer",framebuffer_size_callback);
+    HellowWorld();
     
     Camera camera = Camera(800,600,90.0);
     camera.SetPosition(glm::vec3(0.0f, 0.0f, 1.0f));
@@ -88,12 +105,64 @@ int main()
     
     Model model = Model("Assets/Models/NanoSuit/nanosuit.obj",&phongMat,Transform(glm::vec3(0.0f, 0.0f, -10.0f), glm::vec3(45.0f, 0.0f, 0.0f), glm::vec3(0.3f)));
 
+    unsigned int fbo;
+    glGenFramebuffers(1, &fbo);
+    glBindFramebuffer(GL_FRAMEBUFFER, fbo);
 
-    glEnable(GL_DEPTH_TEST); // 启用深度测试
-    glDepthFunc(GL_LESS);    // 设置深度测试函数为 LESS
-    glDepthMask(GL_TRUE);    // 启用深度写入
-    glDisable(GL_CULL_FACE);
-    glEnable(GL_BLEND);
+    unsigned int texture;
+    glGenTextures(1, &texture); // 生成纹理
+    glBindTexture(GL_TEXTURE_2D, texture); // 绑定纹理
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, 800, 600, 0, GL_RGB, GL_UNSIGNED_BYTE, NULL); // 定义纹理大小,格式,数据
+    //图象从纹理图象空间映射到帧缓冲图象空间(映射需要重新构造纹理图像,这样就会造成应用到多边形上的图像失真),这时就可用glTexParmeteri()函数来确定如何把纹理象素映射成像素.
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR); // 设置纹理缩小过滤方式,LINEAR表示线性插值
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR); // 设置纹理放大过滤方式,LINEAR表示线性插值
+    glBindTexture(GL_TEXTURE_2D, 0); // 解绑纹理
+    
+    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, texture, 0);// 将纹理attach到帧缓冲对象上
+
+    unsigned int rbo;
+    glGenRenderbuffers(1, &rbo); // 生成渲染缓冲
+    glBindRenderbuffer(GL_RENDERBUFFER, rbo); // 绑定渲染缓冲
+    glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH24_STENCIL8, 800, 600);// 定义渲染缓冲大小,格式,数据
+    glBindRenderbuffer(GL_RENDERBUFFER, 0); // 解绑渲染缓冲
+    glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_RENDERBUFFER, rbo); // 将渲染缓冲attach到帧缓冲对象上
+
+    if(glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
+    {
+        std::cout << "Framebuffer not complete!" << std::endl;
+        throw std::runtime_error("Framebuffer not complete!");
+    }
+    glBindFramebuffer(GL_FRAMEBUFFER, 0);
+
+    float quadVertices[] = { // vertex attributes for a quad that fills the entire screen in Normalized Device Coordinates.
+        // positions   // texCoords
+        -1.0f,  1.0f,  0.0f, 1.0f,
+        -1.0f, -1.0f,  0.0f, 0.0f,
+         1.0f, -1.0f,  1.0f, 0.0f,
+
+        -1.0f,  1.0f,  0.0f, 1.0f,
+         1.0f, -1.0f,  1.0f, 0.0f,
+         1.0f,  1.0f,  1.0f, 1.0f
+    };
+    unsigned int quadVAO, quadVBO;
+    glGenVertexArrays(1, &quadVAO);
+    glGenBuffers(1, &quadVBO);
+    glBindVertexArray(quadVAO);
+    glBindBuffer(GL_ARRAY_BUFFER, quadVBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(quadVertices), &quadVertices, GL_STATIC_DRAW);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)(2 * sizeof(float)));
+
+    Shader screenShader = Shader(
+        "Source/GLSL_Shaders/ScreenShader/vert.glsl",
+        "Source/GLSL_Shaders/ScreenShader/frag.glsl",
+        "ScreenShader"
+    );
+
+    SetOpenGL();
+    int frameCount = 0;
     //渲染循环
     while(!glfwWindowShouldClose(window))//窗口应该关闭时结束循环
     {
@@ -103,11 +172,24 @@ int main()
         //输入
         Singleton<InputSystem>::Instance().Update();
 
+        glBindFramebuffer(GL_FRAMEBUFFER, fbo);
         //渲染
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);//设置清空屏幕使用的颜色,是一个状态设置函数
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);//清空颜色缓冲,是一个状态使用函数,获取状态后执行
 
         model.Draw();
+
+        glBindFramebuffer(GL_FRAMEBUFFER, 0);
+        glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        glActiveTexture(GL_TEXTURE0)
+        glBindTexture(GL_TEXTURE_2D, texture);
+        screenShader.Use();
+        unsigned short textureLoc = glGetUniformLocation(screenShader.shaderProgramID, "screenTexture");
+        glUniform1i(textureLoc, GL_TEXTURE0);
+        glBindVertexArray(quadVAO);
+        glDisable(GL_DEPTH_TEST);
+        glDrawArrays(GL_TRIANGLES, 0, 6);
 
         //交换缓冲,检查并调用事件回调函数
         glfwSwapBuffers(window); //交换颜色缓冲
@@ -119,6 +201,7 @@ int main()
         // {
         //     std::cerr << "OpenGL error: " << err << std::endl;
         // }
+        std::cout << "frameCount: " << ++frameCount << std::endl;
     }
 
     glfwTerminate(); //终止glfw
